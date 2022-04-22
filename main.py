4dd8e831d9ec7ea03979a8b1ea012f2b185d6359
JustAPie <justapie06@gmail.com>
2022-04-22 13:02:44 +0700

    Added features for arbitary length words

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ chosen = rand.choice(words).capitalize()
 
 def check_correct_word(str, attempt):
     if str == chosen:
-        print("You have guessed the right word!")
+        print("Congratulation! You have guessed the right word!")
         exit()
     else:
         hint = ""
@@ -35,7 +35,7 @@ def check_correct_word(str, attempt):
         return f'Incorrect words after 5 attempts. The correct word is {chosen}'
 
 
-print("You have 5 attemps to guess the correct 5-character word")
+print(f'You have 5 attemps to guess the correct {len(chosen)}-character word')
 print(
     "For each word, if the answer is incorrect, your hint will appear in a form of word like below"
 )
@@ -50,10 +50,9 @@ print(
     f'{bcolors.GREEN} {bcolors.RESET}: The character is in the answer and have the exact position'
 )
 print("Happy Playing!")
-lim = 5
-for i in range(lim):
+for i in range(5):
     wInput = input()
-    while len(wInput) != 5:
-        print("Please only enter a word with 5 characters")
+    while len(wInput) != len(chosen):
+        print(f'Please only enter a word with {len(chosen)} characters')
         wInput = input()
     check_correct_word(wInput.capitalize(), i)
